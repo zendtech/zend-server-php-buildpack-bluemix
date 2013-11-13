15d665354bbe621c6489444b26dfe1533f4f7652
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2013-11-13 17:51:33 +0200

    ZCLOUD-106 Add cluster support using cleardb

---
+++ json-env-extract.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env /app/bin/php
+<?php
+
+$var = $argv[1];
+$path_str = $argv[2];
+
+$arr = json_decode(getenv($var),true);
+$path = explode('.',$path_str);
+$result = $arr;
+
+foreach($path as $key) {
+    if(isset($result[$key])) {
+        $result = $result[$key];
+    } else {
+        fwrite(STDERR,"Failed locating subkey $key");
+        exit(1);
+    }
+}
+
+echo $result;
