55e5f23deb1610769e8c5c789972d9983496e3cd
dror-g <dror.g@zend.com>
2013-12-26 12:40:07 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ sed "s/vcap/${ZEND_UID}/" ${PHP_INI_SCAN_DIR}/ZendGlobalDirectives.ini.erb > ${P
 sed "s/VCAP_PORT/${PORT}/" /app/nginx/conf/sites-available/default.erb > /app/nginx/conf/sites-available/default
 
 #replace zend-server-6-php-5.4/share/alias-nginx.tpl with one compatible with ZF2
-cat /app/nginx/conf/alias-nginx.tpl > /app/zend-server-6-php-5.4/share/alias-nginx.tpl
+#cat /app/nginx/conf/alias-nginx.tpl > /app/zend-server-6-php-5.4/share/alias-nginx.tpl
 
 rm -rf /app/nginx/conf/sites-enabled
 mkdir -p /app/nginx/conf/sites-enabled
