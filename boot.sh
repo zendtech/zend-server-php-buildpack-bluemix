681a702ddd84ec3edde868c213a856d7654a306e
David Lowes <david.l@zend.com>
2014-01-07 16:27:06 +0200

    Add some debugging info

--- boot.sh
+++ boot.sh
@@ -6,5 +6,12 @@
 # This allows apache to be safely restarted by Zend Server (during deploy etc...).
 echo "Keep container alive..."
 
+#Debugging info
+hostname
+/usr/bin/id
+grep uid /app/zend-server-6-php-5.4/etc/conf.d/ZendGlobalDirectives.ini
+
+###/Debugging info
+
 # Keep the app alive so that the health managager does not kill it
 exec /app/zend-server-6-php-5.4/bin/donothing
