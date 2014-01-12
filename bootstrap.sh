c5065914d5771ba2f21d4434ab48eda694bdb90b
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2014-01-12 17:57:55 +0200

    Update nginx document root in alias.tpl used as template for ZDD deployed apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,7 @@ sed "s/VCAP_PORT/${PORT}/" /app/nginx/conf/sites-available/default.erb > /app/ng
 # Change document root if needed
 if [[ -n $ZEND_DOCUMENT_ROOT ]]; then
     sed -i -e "s|root[ \t]*/app/www|root /app/www/$ZEND_DOCUMENT_ROOT|" /app/nginx/conf/sites-available/default
+    sed -i -e "s|root[ \t]*/app/www|root /app/www/$ZEND_DOCUMENT_ROOT|" /app/nginx/conf/alias-nginx.tpl
 fi
 
 #replace zend-server-6-php-5.4/share/alias-nginx.tpl with one compatible with ZF2
