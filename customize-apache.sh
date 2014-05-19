55467ffb23f7fb209de9f6ae2d6e1cd2220e43dd
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2014-05-19 21:31:15 +0300

    Remove post_activate.php when using apache

--- customize-apache.sh
+++ customize-apache.sh
@@ -21,3 +21,6 @@ fi
 # Change ServerName
 SERVER_NAME=`/app/bin/json-env-extract.php VCAP_APPLICATION application_uris 0`
 sed -i -e "s/localhost.localdomain/$SERVER_NAME/g" /app/apache/etc/apache2/sites-available/default
+
+# Remove post_activate.php
+rm -f /app/zend-server-6-php-5.4/etc/conf.d/post_activate.php
